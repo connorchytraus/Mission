6335be47cae966553c9e422de8787624f9b9f6d1
Connor Chytraus <connorchyt@gmail.com>
2023-02-06 13:56:05 -0700

    mission 5

---
+++ Mission4/Models/GradeCalcModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission4.Models
+{
+    public class GradeCalcModel
+    {
+        [Required]
+        [Range(0,100)]
+        public Int32 assi { get; set; }
+        [Required]
+        [Range(0, 100)]
+        public Int32 grou { get; set; }
+        [Required]
+        [Range(0, 100)]
+        public Int32 quiz { get; set; }
+        [Required]
+        [Range(0, 100)]
+        public Int32 midt { get; set; }
+        [Required]
+        [Range(0, 100)]
+        public Int32 fina { get; set; }
+        [Required]
+        [Range(0, 100)]
+        public Int32 inte { get; set; }
+    }
+}
